a5e8662ce15378cf3b7515254f1a9f262550fdb7
Deepali Ghorpade <ghdeepali77@gmail.com>
2016-11-14 16:25:08 +0530

    Create Code.sh

---
+++ Code.sh
@@ -0,0 +1,33 @@
+// This shell script is developed to get block device details, disk details, searching file, and getting status of searched file with details
+
+echo -e "\n Lists all BLOCK DEVICES"
+lsblk
+
+echo -e " \n To Get  DISK DETAILS\nPress Enter" 
+read key
+fdisk -l
+
+echo -e "\nEnter filename To Get  Information about it"
+read filename
+echo
+   
+echo -e " \nTo get WHERE FILE $filename IS STORED\nPress Enter"
+read key	
+df $filename
+
+echo -e " \nTo get STATUS OF FILE\nPress Enter"
+read key
+
+echo -e "\n Get File Properties \n PRESS ENTER"
+ls -l $filename
+stat -f $filename
+ 
+echo
+
+stat $filename
+
+
+
+ 
+
+   
